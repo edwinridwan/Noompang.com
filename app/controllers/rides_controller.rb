1f2e89f49e65506b64d369502880e01a3611a335
David Haber <=>
2012-06-04 14:55:05 +0200

    User is now redirected to his page after successful creation of ride

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -9,21 +9,14 @@ class RidesController < ApplicationController
 
   def create
     @ride = Ride.new(params[:ride])
-    logger.info "*****DEBUG: " + params[:ride][:start_date]
-    logger.info "*****DEBUG: " + @ride.start_date.to_s
-    logger.info "*****DEBUG: " + params[:ride][:end_time]
-    logger.info "*****DEBUG: Start time:" + @ride.start_time.to_s
-    logger.info "*****DEBUG: End time:" + @ride.end_time.to_s
-
-
-    
+    #logger.info "*****DEBUG: " + params[:ride][:start_date]
     if @ride.save
       # Handle a successful save
       flash[:success] = "Ride successfully created"
       redirect_to @ride
     else
       # Unsuccessful save
-      render 'new'
+      render 'users/show'
     end
   end
 
