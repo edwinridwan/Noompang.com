88a9d94fd5b5bcc8ed720354c2b6a2af1ef6704c
David Haber <=>
2012-06-05 14:29:28 +0200

    Implemented conversion from address to coordinates
    Coordinates of start and end address are now stored in database

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -24,7 +24,9 @@
 #
 
 class Ride < ActiveRecord::Base
-  attr_accessible :user_id, :start_address, :end_address, :start_date, :start_time, :end_date, :end_time, :distance_in_meters
+  attr_accessible :user_id, :start_address, :end_address, :start_date, 
+                  :start_time, :end_date, :end_time, :distance_in_meters, 
+                  :start_lat, :start_long, :end_lat, :end_long
   belongs_to :driver, :class_name => "User"
   has_many   :ride_requests
 
