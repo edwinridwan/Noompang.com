d5f21051466e7e4a3e208d48d61a1f177884c981
David Haber <=>
2012-06-14 16:57:33 +0200

    Removed Users (index) page from header menu

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,14 @@
 module UsersHelper
 
-  # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.email, class: "gravatar")
+  def user_icon_for(user)
+    if (user && !user.image_url.blank?)
+      image_tag(user.image_url, alt: user.first_name, class: "user_icon", size: "20x20")
+    end
   end
 
-  def user_icon_for(user)
-    if (!user.image_url.blank?)
-      image_tag(user.image_url, alt: user.first_name, class: "user_icon", size: "20x20")
+  def user_image_for(user, height, width)
+    if (user && !user.image_url.blank?)
+      image_tag(user.image_url, :height => height, :width => width)
     end
   end
 
