319ca6e359365af8e32ad507aba0b0363f84280f
David Haber <=>
2012-06-23 14:25:09 +0800

    Rearranged user settings page and removed sample contact text

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,12 +41,16 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
-      sign_in @user
-      redirect_to @user
+    if params[:commit].eql?('Save changes')
+      if @user.update_attributes(params[:user])
+        flash[:success] = "Profile updated"
+        sign_in @user
+        redirect_to @user
+      else
+        render 'edit'
+      end
     else
-      render 'edit'
+      redirect_to @user
     end
   end
 
